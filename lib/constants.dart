3a229768404d048a7cf3ec3688c28519b2f68438
Aditya <tejpaladitya@gmail.com>
2023-04-08 21:34:09 +0530

    v-- 0.0.3

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,42 @@
+import 'package:alpha/components/containers.dart';
 import 'package:flutter/material.dart';
 
 const kstartingpagecolor = Color(0xFF021221);
 const kscaffoldcolor = Color(0xFf021221);
+const kcontainerColor = Color(0xFF1D1E33);
+
+const kRecommendTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 25,
+  color: kcontainerColor,
+);
+
+const kconatiner1theme = BoxDecoration(
+  gradient: LinearGradient(
+    colors: [
+      Colors.green,
+      Colors.greenAccent,
+      Colors.cyanAccent,
+      Colors.cyan,
+      Colors.lightBlueAccent,
+      Colors.lightBlue,
+      Colors.blue,
+      Colors.blueAccent,
+    ],
+    begin: Alignment.bottomLeft,
+    end: Alignment.topLeft,
+  ),
+);
+
+const kconatiner2theme = BoxDecoration(
+  gradient: LinearGradient(
+    colors: [
+      Colors.deepPurpleAccent,
+      Colors.purpleAccent,
+      Colors.pinkAccent,
+      Colors.redAccent,
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  ),
+);
--- lib/screens/starting_page.dart
+++ lib/screens/starting_page.dart
@@ -7,18 +7,140 @@ import '../components/containers.dart';
 
 class startingpage extends StatelessWidget {
   startingpage({Key? key}) : super(key: key);
-  final _advancedDrawerController = AdvancedDrawerController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kscaffoldcolor,
       appBar: AppBar(
         title: Text('title'),
         backgroundColor: kstartingpagecolor,
       ),
       drawer: drawer1(),
-      body: conatiner1(),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: kconatiner1theme,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100,
+                decoration: BoxDecoration(color: Colors.white),
+                child: Center(
+                  child: Text(
+                    'Recommended:-',
+                    style: kRecommendTextStyle,
+                  ),
+                ),
+              ),
+              Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: Column(
+                    children: [
+                      InkWell(
+                        splashColor: Colors.blueGrey,
+                        onTap: () {
+                          print('2');
+                        },
+                        child: Stack(
+                          children: [
+                            Image(
+                              image: AssetImage('images/1page.jpg'),
+                              height: 240,
+                              width: double.maxFinite,
+                              fit: BoxFit.cover,
+                            ),
+                            Positioned(
+                              bottom: 16,
+                              right: 16,
+                              left: 16,
+                              child: Text(
+                                'city',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )),
+              Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Column(
+                  children: [
+                    Stack(
+                      children: [
+                        Image(
+                          image: AssetImage('images/1page.jpg'),
+                          height: 240,
+                          width: double.maxFinite,
+                          fit: BoxFit.cover,
+                        ),
+                        Positioned(
+                          bottom: 16,
+                          right: 16,
+                          left: 16,
+                          child: Text(
+                            'city',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Column(
+                  children: [
+                    Stack(
+                      children: [
+                        Image(
+                          image: AssetImage('images/1page.jpg'),
+                          height: 240,
+                          width: double.maxFinite,
+                          fit: BoxFit.cover,
+                        ),
+                        Positioned(
+                          bottom: 16,
+                          right: 16,
+                          left: 16,
+                          child: Text(
+                            'city',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       bottomNavigationBar: bottomappBar(),
     );
   }
